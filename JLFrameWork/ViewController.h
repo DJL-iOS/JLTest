4f560619a55ab9a791870fe09c5254f59ce4e17e
jinlong_dou <jinlong.dou@nexttao.com>
2020-03-12 22:26:46 +0800

    项目提交

---
+++ JLFrameWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JLFrameWork
+//
+//  Created by DJL on 2020/3/12.
+//  Copyright © 2020 DJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JLFrameWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JLFrameWork
+//
+//  Created by DJL on 2020/3/12.
+//  Copyright © 2020 DJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
